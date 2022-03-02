73565c9c9572f1898239679f6b5fc7a96a448162
MOA <moyo.ajla@gmail.com>
2022-03-02 22:54:21 +0100

    first commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void play (View v){
+        v.setEnabled(false);
+        //R.id.play
+        Log.d("success","disabled");
+    }
+
+}
+
