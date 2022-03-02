cb05973bcbaa8d42969c7f198373cd317242ddbd
MOA <moyo.ajla@gmail.com>
2022-03-02 23:23:31 +0100

    Second commit

---
+++ app/src/main/java/com/example/myapplication/LocalPlay.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LocalPlay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_local_play);
+    }
+}
