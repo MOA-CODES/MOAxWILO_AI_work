d7420543bc906e315ff325a6f435be242169e321
MOA <moyo.ajla@gmail.com>
2022-03-03 18:05:22 +0100

    Fourth commit

--- app/src/main/java/com/example/myapplication/PVP.java
+++ app/src/main/java/com/example/myapplication/PVP.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class PVP extends AppCompatActivity {
 
@@ -17,6 +19,23 @@ public class PVP extends AppCompatActivity {
     public void launchHome(View v){
         Intent i = new Intent(this, MainActivity.class);
         startActivity(i);
+    }
+
+
+    public void launchPVParena(View v){
+        EditText a = findViewById(R.id.inputp1);
+        EditText b = findViewById(R.id.inputp2);
+      String p1 = a.getText().toString();
+      String p2 = b.getText().toString();
+
+        if(((p1.isEmpty())) && ((p2.isEmpty()))){
+            Toast.makeText(this, "input p1 and p2", Toast.LENGTH_LONG).show();//alerting
+        }else{
+            Intent i = new Intent(this, Pvparena.class);
+            i.putExtra("p1", p1);
+            i.putExtra("p2", p2);
+            startActivity(i);
+        }
     }
 
 }
