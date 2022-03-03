efc81292bbd2f22e2e6a9d64cb26954ede878293
MOA <moyo.ajla@gmail.com>
2022-03-03 02:37:19 +0100

    Third commit

--- app/src/main/java/com/example/myapplication/LocalPlay.java
+++ app/src/main/java/com/example/myapplication/LocalPlay.java
@@ -2,7 +2,9 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LocalPlay extends AppCompatActivity {
 
@@ -11,4 +13,15 @@ public class LocalPlay extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_local_play);
     }
+
+    public void launchHome(View v){
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+    }
+
+    public void launchPVP(View v){
+        Intent i = new Intent(this, PVP.class);
+        startActivity(i);
+    }
+
 }
--- app/src/main/java/com/example/myapplication/PVP.java
+++ app/src/main/java/com/example/myapplication/PVP.java
@@ -4,31 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class PVP extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_p_v_p);
     }
 
-    public void play (View v){
-        v.setEnabled(false);
-        //R.id.play
-        Log.d("success","disabled");
-    }
-
-    public void launchlocalplay(View v){
-        Intent i = new Intent(this, LocalPlay.class);
-        startActivity(i);
-    }
     public void launchHome(View v){
         Intent i = new Intent(this, MainActivity.class);
         startActivity(i);
     }
 
 }
-
